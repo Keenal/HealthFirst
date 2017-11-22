53cf3e6aba17b9f65639b88adf5c523a4ac2ae3b
Timothy McWatters <timmch2o@gmail.com>
2017-11-21 22:36:00 -0600

    update Patient class. Add findPrescription() deletePrescriptionAwaitingVerification() methods to ensure after a prescription is verified it is not still in que for verification

--- src/patientprocessing/Patient.java
+++ src/patientprocessing/Patient.java
@@ -25,6 +25,7 @@ public class Patient {
 
 	private static int PRESCRIPTION_LIST_SIZE = 50;
 	private static int DOSE_GIVEN_LIST_SIZE = 100;
+	private static int NOTFOUND = -1;
 	
 	private String name = "";
 	private int patientID = 0;
@@ -253,12 +254,60 @@ public class Patient {
 	} // end of addPrescription method
 	
 	/**
+	 * attempts to find a prescription in the prescriptionsAwaitingVerification array, returns NOTFOUND constant if not found
+	 * @param precriptionToFind = the prescriptio to find
+	 * @return int = the array index number that was found, or NOTFOUND constant if the prescription was not found
+	 */
+	private int findPrescription(Medication precriptionToFind) {
+		for (int indexNumber = 0; indexNumber < prescriptionsAwaitingVerification.length; indexNumber++) {
+			if (prescriptionsAwaitingVerification[indexNumber] == null) {
+				continue;
+			}
+			else if (prescriptionsAwaitingVerification[indexNumber].getName().equalsIgnoreCase(precriptionToFind.getName())) {
+				return indexNumber;
+			}
+			else { 
+				continue;
+			}
+		}
+		return NOTFOUND;
+	} // end of findPrescription method
+	
+	/**
+	 * uses findPrescription() method to find and delete an prescriptionsAwaitingVerification after it is verified
+	 * this method also re-organizes the users array to get rid of null or empty objects in the middle of the array 
+	 * @param precriptionToDelete = the prescription to delete
+	 * @return boolean = true if account was found and deleted, or false if not
+	 */
+	private boolean deletePrescriptionAwaitingVerification(Medication precriptionToDelete) {
+		int findPrescriptionResult = findPrescription(precriptionToDelete);
+		int j = 0;
+		if (findPrescriptionResult >= 0) {
+			prescriptionsAwaitingVerification[findPrescriptionResult] = null;
+			for (int i = 0; i < prescriptionsAwaitingVerification.length; i++) {
+				if ((prescriptionsAwaitingVerification[i] != null) && !(prescriptionsAwaitingVerification[i].getName().equalsIgnoreCase(""))) {
+					prescriptionsAwaitingVerification[j] = prescriptionsAwaitingVerification[i];
+					j++;
+				}
+			}
+			numOfPrescriptionsAwaitingVerification = j;
+			for (int k = j; k < prescriptionsAwaitingVerification.length; k++) {
+				prescriptionsAwaitingVerification[k] = null;
+			}
+			return true;
+		} else {
+			return false;
+		}
+	} // end of deletePrescriptionAwaitingVerification method
+	
+	/**
 	 * adds a prescription to the patients activePrescriptionsVerified array 
 	 * and increments the numOfPrescriptionsVerified instance variable by 1
 	 * @param prescriptionAwaitingVerification = the prescription to verify
 	 */
 	public void verifyPrescription(Medication prescriptionAwaitingVerification) {
 		activePrescriptionsVerified[numOfPrescriptionsVerified++] = prescriptionAwaitingVerification;
+		deletePrescriptionAwaitingVerification(prescriptionAwaitingVerification);
 	} // end of addPrescription method
 	
 	/* returns a nicely formatted String representing the history of the medication given to the patient
