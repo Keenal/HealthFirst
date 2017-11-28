e4c713256a88fc3ddb265317d89acbc1846736c1
keenal <keenalmshah@gmail.com>
2017-11-28 15:12:01 -0600

    create UserTestCase, test setName and setPid

--- src/test/UserTest.java
+++ src/test/UserTest.java
@@ -1,14 +1,38 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class UserTest {
+import users.User;
 
+public class UserTest {
+
+	User user = new User();
+
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testSetName() {
+		user.setName("test");
+		assertEquals("name", "test", user.getName());
 	}
+	
+	@Test
+	public void testSetPid() {
+		user.setPid(1);
+		assertEquals("pid", 1, user.getPid());
+		
+	}
+	
+	/*
+	@Test
+	public void test_toString_returnsUserName() {
+		String result = "User [name =" +  + "]";
+		assertEquals(user.toString(), result);
+	}
+	*/
+	
+	
 
 }
