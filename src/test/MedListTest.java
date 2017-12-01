5cb14a56888a2b28f92880fc9631a01fd45be825
keenal <keenalmshah@gmail.com>
2017-12-01 01:46:41 -0600

    add a bunch of test classes

---
+++ src/test/MedListTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import medicationprocessing.MedicationList;
+
+class MedListTest {
+	
+	MedicationList list = new MedicationList();
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/MedTest.java
@@ -0,0 +1,93 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import medicationprocessing.Medication;
+
+class MedTest {
+	
+	Medication med = new Medication();
+
+// test medication name
+	@Test
+	public void testSetName() {
+		
+		med.setName("test");
+		assertEquals("name", "test", med.getName());
+		
+	}
+	
+// test description
+	@Test
+	public void testSetDescription() {
+		
+		med.setDescription("test");
+		assertEquals("description", "test", med.getDescription());
+		
+	}
+	
+// test dosage
+	@Test
+	public void testDosage() {
+		
+		med.setDosageInMg(5);
+		assertEquals("dosage", 5, med.getDosageInMg());
+		
+	}	
+	
+// test minDosage
+	@Test
+	public void testSetMinDosage() {
+		
+		med.setMinDosageTimeHours(5);
+		assertEquals("Mindosage", 5, med.getMinDosageTimeHours());
+		
+	}	
+	
+// test if the drug is narcotic
+	@Test
+	public void test_ifNarcotic() {
+		
+		med.setNarcotic(true);
+		assertEquals("narcotic", true, med.isNarcotic());
+		
+	}	
+	
+// test if the drug is over the counter
+	@Test
+	public void test_ifOverTheCounter() {
+		
+		med.setOverTheCounter(true);
+		assertEquals("counter", true, med.isOverTheCounter());
+		
+	}	
+	
+// test if the drug is antibiotic
+	@Test
+	public void test_ifAntibiotic() {
+		
+		med.setAntibiotic(true);
+		assertEquals("antibiotic", true, med.isAntibiotic());
+		
+	}	
+	
+// test if the drug is antiInflamitory
+	@Test
+	public void test_ifAntiInflamitory() {
+		
+		med.setAntiInflamitory(true);
+		assertEquals("antiInfamitory", true, med.isAntiInflamitory());
+		
+	}	
+	
+// test toString method
+	@Test
+	public void test_toString_returnsMedInfo() {
+		
+	
+		
+	}
+}
