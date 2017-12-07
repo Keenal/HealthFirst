9c5af13d531983667254aa263c459ddd0eb3d1c4
Timothy McWatters <timmch2o@gmail.com>
2017-12-07 16:31:49 -0600

    finished PDoseTest JUnit Tests

--- src/test/PDoseTest.java
+++ src/test/PDoseTest.java
@@ -1,21 +1,19 @@
 package test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import java.util.Calendar;
 import java.util.Date;
-
 import org.junit.jupiter.api.Test;
-
 import medicationprocessing.Medication;
+import patientprocessing.Patient;
+import patientprocessing.PatientAccounts;
 import patientprocessing.PatientDose;
 
 class PDoseTest {
-	
 	PatientDose dose = new PatientDose();
 	Medication med = new Medication();
 	Date timeDoseLogged = null;
 	Date nextDoseDue = null;
-	
 
 	// test medication given
 	@Test
@@ -23,7 +21,6 @@ class PDoseTest {
 
 		dose.setMedicationGiven(med);
 		assertEquals(med, dose.getMedicationGiven());
-		
 	}
 	
 	// test setting dose time
@@ -32,7 +29,6 @@ class PDoseTest {
 
 		dose.setTimeDoseLogged(timeDoseLogged);
 		assertEquals(timeDoseLogged, dose.getTimeDoseLogged());
-		
 	}
 	
 	// test setting next dose
@@ -41,7 +37,6 @@ class PDoseTest {
 
 		dose.setNextDoseDue(nextDoseDue);
 		assertEquals(nextDoseDue, dose.getNextDoseDue());
-		
 	}
 	
 	// test nurseWhoLogged
@@ -50,7 +45,6 @@ class PDoseTest {
 
 		dose.setNurseWhoLogged("Mary");
 		assertEquals("Mary", dose.getNurseWhoLogged());
-		
 	}
 	
 	// test amount of dose given
@@ -59,21 +53,23 @@ class PDoseTest {
 
 		dose.setDoseGivenInMg(5);
 		assertEquals(5, dose.getDoseGivenInMg());
-		
-	}
+	} 
 	
-	/* FAILing
-	// test setting next dose
+	// test toString
 	@Test
 	void test_toString_returns_PatientDose() {
-
-		String result = dose.toString();
-		assertTrue(result.contains("[" + med.getName() + "]  -  "  + "Time last dose given: " + timeDoseLogged
-				+ ", Time next dose due: " + nextDoseDue + ", Given by: " + dose.getNurseWhoLogged() + ", Mg given: "
-				+ dose.getDoseGivenInMg()));
-	}
-	*/
-	
-	
-
+		new PatientAccounts();
+		Patient patient = new Patient("Jane Doe",0,"Phil",20, "200 / 50",90, 155.5, 70.2, "Lots of drugs", "flu","Pollen");
+		PatientAccounts.addPatient(patient);
+		Medication medicationGiven = new Medication("testMed", "testDescription", 0, 1, false, true, false, false);
+		Calendar cal = Calendar.getInstance();
+		Date timeDoseLogged = cal.getTime();
+		int minHoursTillDue = medicationGiven.getMinDosageTimeHours();
+		cal.add(Calendar.HOUR, minHoursTillDue);
+		Date nextDoseDue = cal.getTime();
+		PatientDose dose = new PatientDose(medicationGiven, timeDoseLogged, nextDoseDue, "Linda Carter", 350);
+		String expectedResult = dose.toString();
+		String result = "[" + medicationGiven.getName() + "]  -  " + "Time last dose given: " + timeDoseLogged + ", Time next dose due: " + nextDoseDue + ", Given by: " + "Linda Carter" + ", Mg given: " + 350;
+		assertEquals(expectedResult, result);
+	} // end of test_toString_returns_PatientDose method
 }
