22b195e4eef2125f86210bbe5cf8fd4787df2a8b
Timothy McWatters <timmch2o@gmail.com>
2017-12-08 11:14:44 -0600

    finished PatientTest JUnit Tests

--- src/test/PatientTest.java
+++ src/test/PatientTest.java
@@ -1,14 +1,8 @@
 package test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Date;
-
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
-
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
@@ -16,8 +10,6 @@ import javafx.beans.property.StringProperty;
 import medicationprocessing.Medication;
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
-
-import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
 import patientprocessing.Patient;
 import patientprocessing.PatientDose;
@@ -29,16 +21,13 @@ class PatientTest {
 		Patient patient = new Patient();
 		StringProperty stringP = new SimpleStringProperty("test");
 		patient.setName("test");
-		
 		assertEquals(patient.getName(), stringP.getValue());
 	}
-	
 	
 	@Test
 	public void setNameTest() {
 		Patient patient = new Patient();
 		patient.setName("test");
-	
 		assertEquals("test", patient.getName());
 	}
 	
@@ -47,7 +36,6 @@ class PatientTest {
 		Patient patient = new Patient();
 		StringProperty stringP = new SimpleStringProperty("test");
 		patient.setPrimaryDoctorName("test");
-		
 		assertEquals(patient.getPrimaryDoctorName(), stringP.getValue());
 	}
 	
@@ -55,7 +43,6 @@ class PatientTest {
 	public void setPrimaryDoctorNameTest() {
 		Patient patient = new Patient();
 		patient.setPrimaryDoctorName("test");
-		
 		assertEquals("test", patient.getPrimaryDoctorName());
 	}
 	
@@ -64,7 +51,6 @@ class PatientTest {
 		Patient patient = new Patient();
 		IntegerProperty intP = new SimpleIntegerProperty(0);
 		patient.setAge(0);
-		
 		assertEquals(patient.getAge(), intP.getValue());
 	}
 	
@@ -73,16 +59,14 @@ class PatientTest {
 		IntegerProperty intP = new SimpleIntegerProperty(21);
 		Patient patient = new Patient();
 		patient.setAge(intP.getValue());
-		
 		assertEquals(patient.getAge(), intP.getValue().intValue(),0);
-
 	}
+	
 	@Test
 	public void getPatientIDPropertyTest() {
 		Patient patient = new Patient();
 		IntegerProperty intP = new SimpleIntegerProperty(0);
 		patient.setPatientID(0);
-		
 		assertEquals(patient.getPatientID(), intP.getValue());
 	}
 	
@@ -91,15 +75,14 @@ class PatientTest {
 		IntegerProperty intP = new SimpleIntegerProperty(1);
 		Patient patient = new Patient();
 		patient.setPatientID(intP.getValue());
-		
 		assertEquals(patient.getPatientID(), intP.getValue().intValue(),0);
 	}
+	
 	@Test
 	public void getBloodPressurePropertyTest() {
 		Patient patient = new Patient();
 		StringProperty stringP = new SimpleStringProperty("test");
 		patient.setBloodPressure("test");
-		
 		assertEquals(patient.getBloodPressure(), stringP.getValue());
 	}
 	
@@ -115,7 +98,6 @@ class PatientTest {
 		Patient patient = new Patient();
 		IntegerProperty intP = new SimpleIntegerProperty(0);
 		patient.setHeartRate(0);
-		
 		assertEquals(patient.getHeartRate(), intP.getValue());
 	}
 	
@@ -125,14 +107,13 @@ class PatientTest {
 		Patient patient = new Patient();
 		patient.setHeartRate(intP.getValue());
 		assertEquals(patient.getHeartRate(), intP.getValue().intValue(),0);
-
 	}
+	
 	@Test
 	public void getWeightInLbsPropertyTest() {
 		Patient patient = new Patient();
 		DoubleProperty doubleP = new SimpleDoubleProperty(0.0);
 		patient.setWeightInLbs(0.0);
-		
 		assertEquals(patient.getWeightInLbs(), doubleP.getValue());
 	}
 	
@@ -142,8 +123,6 @@ class PatientTest {
 		Patient patient = new Patient();
 		patient.setWeightInLbs(doubleP.doubleValue());
 		assertEquals(patient.getWeightInLbs(), doubleP.getValue().doubleValue(), 0);
-		
-		
 	}
 	
 	@Test
@@ -168,38 +147,37 @@ class PatientTest {
 		Patient patient = new Patient();
 		StringProperty stringP = new SimpleStringProperty("test");
 		patient.setFamilyHistory("test");
-		
 		assertEquals(patient.getFamilyHistory(), stringP.getValue());
 	}
+	
 	@Test
 	public void setFamilyHistoryTest() {
 		Patient patient = new Patient();
 		patient.setFamilyHistory("test");
-		
 		assertEquals("test", patient.getFamilyHistory());
 	}
+	
 	@Test
 	public void getCurrentIllnessPropertyTest() {
 		Patient patient = new Patient();
 		StringProperty stringP = new SimpleStringProperty("test");
 		patient.setCurrentIllness("test");
-		
 		assertEquals(patient.getCurrentIllness(), stringP.getValue());
 	}
+	
 	@Test
 	public void getAllergiesPropertyTest() {
 		Patient patient = new Patient();
 		StringProperty stringP = new SimpleStringProperty("test");
 		patient.setAllergies("test");
-		
 		assertEquals(patient.getAllergies(), stringP.getValue());
 	}
+	
 	@Test
 	public void setCurrentIllnessTest() {
 		Patient patient = new Patient();
 		patient.setCurrentIllness("test");
 		assertEquals("test", patient.getCurrentIllness());
-		
 	}
 	
 	@Test
@@ -207,7 +185,6 @@ class PatientTest {
 		Patient patient = new Patient();
 		patient.setAllergies("test");
 		assertEquals("test", patient.getAllergies());
-		
 	}
 	
 	@Test 
@@ -216,7 +193,6 @@ class PatientTest {
 		Patient patient = new Patient();
 		patient.addDose(patientDose);	
 		assertEquals(patientDose, patient.dosesGivenToPatient[patient.numOfDosesGiven-1]);
-	
 	}
 	
 	@Test
@@ -225,7 +201,6 @@ class PatientTest {
 		Patient patient = new Patient();
 		patient.addPrescription(medication);
 		assertEquals(medication, patient.prescriptionsAwaitingVerification[patient.numOfPrescriptionsAwaitingVerification - 1]);
-		
 	}
 	
 	@Test
@@ -233,13 +208,8 @@ class PatientTest {
 		Medication medication = new Medication("test", "test", 0, 0, false, false, false, false);
 		Patient patient = new Patient();
 		patient.addPrescription(medication);
-		
 		assertEquals(0, patient.findPrescription(medication));
-		
 	}
-	
-	
-	
 
 	@Test
 	public void deletePrescriptionAwaitingVerificationTest() throws NoSuchMethodException,
@@ -247,37 +217,29 @@ class PatientTest {
 		Medication medication = new Medication("test", "test", 500, 4, false, false, false, false);
 		Patient patient = new Patient();
 		boolean deleted = patient.deletePrescriptionAwaitingVerification(medication);
-		
 		assertEquals(false, deleted);
 	}
 
-	
 	@Test
 	public void verifyPrescriptionTest() {
 		Medication medication = new Medication();
 		Patient patient = new Patient();
 		patient.verifyPrescription(medication);
-		
 		assertEquals(medication, patient.activePrescriptionsVerified[patient.numOfPrescriptionsAwaitingVerification]);
 	}
 	
 	@Test
 	public void medicationHistoryTest() {
 		Patient patient = new Patient();
-		
 		assertEquals("", patient.medicationHistory());		
 	}
 
-	
-	/**
-	 * DOES NOT WORK
 	@Test
 	public void toStringTest() {
-		Patient patient = new Patient();
+		Patient patient = new Patient("Jane Doe", 0, "Phil", 20, "200 / 50", 90, 155.5, 70.2, "Lots of drugs", "flu","Pollen");
 		String result = patient.toString();
-		assertTrue(result.contains("Patient [name=" + patient.getName() + ", patientID=" + patient.getPatientID() + ", primaryDoctorName=" + patient.getPrimaryDoctorName() + ", prescriptionsAwaitingVerification=" + patient.numOfPrescriptionsAwaitingVerification + 
-				", numOfPrescriptionsVerified=" + patient.numOfPrescriptionsVerified + "]"));
-		
-	}
-	*/
+		System.out.println(result);
+		assertTrue(result.contains("Patient [name=StringProperty [value: Jane Doe], patientID=IntegerProperty [value: 0], "
+				+ "primaryDoctorName=StringProperty [value: Phil], prescriptionsAwaitingVerification=0, numOfPrescriptionsVerified=0]"));
+	}	
 }
